6813a4d443647b9128815567f11bfcea7be29764
user <user@mail.com>
2024-03-21 16:40:04 -0500

    added vararg example

--- functions.py
+++ functions.py
@@ -2,13 +2,28 @@ def default_param_example(param='defaultValue'):
     print(f'my param is {param}')
 
 
-def returning_function(seed=1) -> int:
+def __returning_function(seed=1) -> int:
     return seed
 
 
 def returning_function_example():
-    print(f'the return value is : {returning_function()}')
+    print(f'the return value is : {__returning_function()}')
 
 
+# Keyword arguments
 def named_param_example():
     default_param_example(param='anotherValue')
+
+
+def __vararg_function(*items: int) -> int:
+    total = 0
+    for item in items:
+        total += item
+    return total
+
+
+# Varargs = arbitrary arguments
+def vargarg_example():
+    print("** integer sum **")
+    total = __vararg_function(4, 54, 1, 6, 7, 12, 34, 123, 3)
+    print(f"the total is: {total}")
--- main.py
+++ main.py
@@ -9,7 +9,8 @@ def menu():
             2. Range
             3. Function with Default Parms
             4. Function with Named Params
-            5. Function Returning Value""")
+            5. Function Returning Value
+            6. Function with VarArgs""")
         selection = input("your selection (anything else will end the program): ")
         if selection == '1':
             control_structure_example()
@@ -21,6 +22,8 @@ def menu():
             named_param_example()
         elif selection == '5':
             returning_function_example()
+        elif selection == '6':
+            vargarg_example()
         else:
             break
         print("\n")
